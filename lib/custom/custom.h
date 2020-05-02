c3b03cee46d7f330d775ffd1f9cd8fe27b9ae42a
mathster101 <mathewsimonpanicker@gmail.com>
2020-05-03 01:44:57 +0530

    .folders should be visible now?

--- lib/custom/custom.h
+++ lib/custom/custom.h
@@ -1,26 +1,40 @@
 #include <Arduino.h>
+#include <MPU9250.h>
+
 
 /*
-* general data container
+ * general data container
 */
 struct telemdata{
   float voltage;
-  float altitude;
+  uint16_t altitude;
   float roll,pitch,yaw;
-  float temp;
+  uint8_t temp;
 } telemetry;
 
-float * offsets();
+int countled = 0;
+float pitch_off,roll_off;
+MPU9250 mpu;
+
+void offsets();
 void led_control();
 float getBattery();
 void printall();
 
 /* 
-*initialize serial and pins
+ *initialize serial comms and pin modes
 */
 void inits(){
     pinMode(A0,INPUT);
+    pinMode(8,OUTPUT); //red led
+    pinMode(7,OUTPUT); //green led
+    digitalWrite(8,HIGH);
+    digitalWrite(7,LOW);
     Serial.begin(115200);
+    for(int i =0;i<10;i++){
+        digitalWrite(7,!digitalRead(7));
+        delay(50);
+    }
 };
 
 /*
@@ -44,8 +58,53 @@ void printall(telemdata a){
 float getBattery(){         
     float level = 0;  
     float vol = analogRead(A0);
-    vol = vol *5 / 1024;
-    vol = (12 + 6.8) * vol /6.8;
+    //vol = vol *5 / 1024;
+    //vol = (12 + 6.8) * vol /6.8;
     level = vol;
     return level;
+}
+
+/*
+ * controls led flashing
+*/
+void led_control(){
+    if(countled == 20){
+        digitalWrite(8,HIGH);
+        countled = 0;
+    }
+    else
+    {
+        countled ++;
+    }
+
+    if(countled<2){
+        digitalWrite(7,HIGH);
+    }
+    else
+    {
+        digitalWrite(7,LOW);
+    }
+    
+    
+}
+
+/*
+ *calculates initial angle offsets
+*/
+void offsets(){
+    int num = 500;
+    roll_off = pitch_off = 0;
+    for(int k = 0;k<200;k++){
+
+        delay(2);
+        mpu.update();
+    }
+    for(int k =0;k<num;k++){
+        mpu.update();
+        roll_off += mpu.getRoll();
+        pitch_off += mpu.getPitch();
+        //delay(2);
+    }
+    roll_off /= num;
+    pitch_off /= num;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include <custom.h>
 
 /* Object initializations*/
-MPU9250 mpu;
+
 RF24 radio(9,10); // CE, CSN
 Adafruit_BMP280 bmp;
 
@@ -17,7 +17,6 @@ const byte address[6] = "00001"; //remember to change
 
 
 void setup() {
-  inits();
   bmp.begin();
   radio.begin();
   radio.openWritingPipe(address);
@@ -29,29 +28,37 @@ void setup() {
                   Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
                   Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_1); /* Standby time. */
+                  Adafruit_BMP280::STANDBY_MS_250); /* Standby time. */
   mpu.setup();
   telemetry.yaw = telemetry.pitch = telemetry.roll = 0;
   telemetry.temp = telemetry.altitude = telemetry.voltage = 0; 
   Serial.println("Initializations complete");
-  delay(1000);
+  inits();
+  offsets();
 }
 
 int state = 0;
 void loop() {
+    float t1 = millis();
     mpu.update();
+    
     switch(state){
-      case 0 : telemetry.altitude = bmp.readAltitude();
-              telemetry.roll = mpu.getRoll();  
+      case 0 : telemetry.altitude = bmp.readAltitude(); 
               state = 1;
               break;
       case 1 : telemetry.temp = bmp.readTemperature();
-              telemetry.yaw = mpu.getYaw();
               state = 0;
               break;
     }
-    telemetry.pitch = mpu.getPitch();
+    
+    telemetry.yaw = mpu.getYaw();
+    telemetry.roll = mpu.getRoll() - roll_off;
+    telemetry.pitch = mpu.getPitch() - pitch_off;
     telemetry.voltage = getBattery();
+    
+    radio.writeFast(&telemetry,sizeof(telemdata));
+    //Serial.println(millis() - t1);
+    led_control();
     printall(telemetry);
-    delay(100);
+    //delay(20);
 }
